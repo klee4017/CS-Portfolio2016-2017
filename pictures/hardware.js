ec170b069f73960dacf32b06cf9349d66efc4cc8
klee4017 <krysten@moeliew.com>
2016-09-02 22:29:43 +0000

    added js page for hardware with variables. Added pics to hardware for links/variables

---
+++ pictures/hardware.js
@@ -0,0 +1,14 @@
+// JavaScript File
+
+var motherboard = document.getElementById("motherboard")
+var cpu = document.getElementById("CPU")
+var ram = document.getElementById("RAM")
+var hardDrive = document.getElementById("harddrive")
+var keyboard = document.getElementById("keyboard")
+var mouse = document.getElementById("mouse")
+var monitor = document.getElementById("monitor")
+
+
+
+
+
