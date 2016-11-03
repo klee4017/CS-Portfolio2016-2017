62f7d3fdc46fcd745faef17b38ff18ec2952a93c
klee4017 <krysten@moeliew.com>
2016-11-03 22:28:52 +0000

    bleh

--- calc.js
+++ calc.js
@@ -1,2 +1,35 @@
 // JavaScript File
 
+var one = document.getElementById("one")
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var five = document.getElementById("five");
+var six = document.getElementById("six");
+var seven = document.getElementById("seven");
+var eight = document.getElementById("eight");
+var nine = document.getElementById("nine");
+var zero = document.getElementById("zero");
+var add = document.getElementById("add");
+var subtract = document.getElementById("subtract");
+var multiply = document.getElementById("multiply");
+var divide = document.getElementById("divide");
+var posNeg = document.getElementById("posNeg");
+var decimal = document.getElementById("decimal");
+var exponent = document.getElementById("exponent");
+var squareRoot = document.getElementById("squareRoot");
+var equals = document.getElementById("equals");
+var display = document.getElementById("display");
+var number = document.getElementsByClassName("number");
+var operator = document.getElementsByClassName("operator");
+var button = document.querySelectorAll("button");
+
+
+equals.addEventListener("click", showEquals);
+number.addEventListener("click", showNumber);
+operator.addEventListener("click", operate)
+button.addEventListener("click", 
+    function showButton (evt.target.button){
+        display.innerHTML
+    }
+)
