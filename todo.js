23a1407e2cfa43e4fb1b29804658b9841f3586e3
klee4017 <krysten@moeliew.com>
2016-11-10 22:45:13 +0000

    added how to page for hardware page. Did not finish how to page for calculator yet.

--- todo.js
+++ todo.js
@@ -1,36 +1,50 @@
 // JavaScript File
-
+//input box
 var input = document.getElementById("input");
+//dropdown with different styling
 var dropdown = document.getElementById("dropdown");
+//button that will move the text when inputted
 var submitButton = document.getElementById("submitButton");
+// area where the text will be displayed
 var display = document.getElementById("display");
 
 
 
-
+//adds whatever is placed in the input below the input box
 submitButton.addEventListener('click', function(){
     
     
     var task = "<p>" + input.value + "<p>";
     display.innerHTML = display.innerHTML + task;
+});
+
+dropdown.addEventListener("click", function(){
+     
     
-   
 });
 
 
+
+
+
+
+
+
+
+//deletes text when it's clicked if it has a strikethrough
 display.addEventListener("click", function(evt){
+    var text=evt.target;
     
-    var text = evt.target;
-    text.parentNode.removeChild(text);
+    if (text.style.textDecoration === "line-through"){
+        
+        text.parentNode.removeChild(text);
+    }
+//adds strikethrough if there isn't one already    
+    else {
+        
+        text.style.textDecoration = "line-through";
+    }
 });
 
 
 
-
-
-
-//  style=" + "text-decoration='" + style + "'
-
- // if (dropdown.value == "underline"){
-//     task.style.textDecoration = "underline";
-// }
