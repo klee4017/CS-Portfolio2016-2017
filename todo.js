4d364ff1e6e857539903baf31332ee781e2b6166
klee4017 <krysten@moeliew.com>
2016-12-16 18:17:22 +0000

    fixed dropdown so that when a color is selected, the text added in the todo list will appear in the color selected

--- todo.js
+++ todo.js
@@ -7,21 +7,53 @@ var dropdown = document.getElementById("dropdown");
 var submitButton = document.getElementById("submitButton");
 // area where the text will be displayed
 var display = document.getElementById("display");
+//todolist variable in case color doesn't show up
+var task = "<p>" + input.value + "<p>";
 
 
 
 //adds whatever is placed in the input below the input box
 submitButton.addEventListener('click', function(){
+    var colorBlack = "<div style='color: black'>" + input.value + "<div>";
+    if (dropdown.value === "black"){
+        display.innerHTML = display.innerHTML + colorBlack;
+    }
+   var colorRed = "<div style='color: red'>" + input.value + "<div>";
+    if (dropdown.value === "red"){
+        display.innerHTML = display.innerHTML + colorRed;
+    }
+    var colorYellow = "<div style='color: yellow'>" + input.value + "<div>";
+    if (dropdown.value === "yellow"){
+        display.innerHTML = display.innerHTML + colorYellow;
+    }
+    var colorGreen = "<div style='color: green'>" + input.value + "<div>";
+    if (dropdown.value === "green"){
+        display.innerHTML = display.innerHTML + colorGreen;
+    }
+    var colorBlue = "<div style='color: blue'>" + input.value + "<div>";
+    if (dropdown.value === "blue"){
+        display.innerHTML = display.innerHTML + colorBlue;
+    }
+    var colorPurple = "<div style='color: purple'>" + input.value + "<div>";
+    if (dropdown.value === "purple"){
+        display.innerHTML = display.innerHTML + colorPurple;
+    }
+    var colorOrange = "<div style='color: orange'>" + input.value + "<div>";
+    if (dropdown.value === "orange"){
+        display.innerHTML = display.innerHTML + colorOrange;
+    }
+    var colorGrey = "<div style='color: grey'>" + input.value + "<div>";
+    if (dropdown.value === "grey"){
+        display.innerHTML = display.innerHTML + colorGrey;
+    }
     
     
-    var task = "<p>" + input.value + "<p>";
-    display.innerHTML = display.innerHTML + task;
+    
+//     display.innerHTML = display.innerHTML + task;
+
+    
 });
 
-dropdown.addEventListener("click", function(){
-     
-    
-});
 
 
 
